8c996af2e49204f46810b4a9c0a6ca3725bfce15
David Augusto <david11augusto11@gmail.com>
2024-07-08 20:19:02 -0400

    aula 1 js

---
+++ JavaScript/aula1.js
@@ -0,0 +1,76 @@
+/*
+const number = 1; //number
+const string = "text"; //string
+const bool = true; // booleano (lógico)
+const nulo = null; 
+const undef = undefined;
+
+const objeto = {
+    nome: "David",
+    idade:22,
+}
+
+objeto.nome = "Augusto";
+
+const array = [5, "três", false, function teste() {
+
+}];
+
+const sum = 4 + 9;
+const sub = 4 - 9;
+const mult = 4 * 9;
+const div = 4 / 9;
+*/
+
+
+/*
+variavel = 123456;
+const constante = "constante" //CONSTANTE;
+
+
+
+let variavel2 = "valor fora"; // escopo local (contexto de bloco)
+function teste(){
+    let variavel2 = "valor dentro";
+    variavel2 = "mudamos os dois"
+    console.log(variavel2);
+}
+
+teste();
+console.log(variavel2)*/
+
+/* 
+
+function teste(){
+    var global = "oi";
+    teste2()
+}
+
+function teste2(){
+    global = "tchau";
+}
+
+teste();
+console.log(global);
+ */
+/* 
+let numero = 22
+if (numero === "22"){
+    console.log("é igual")
+}else{
+    console.log("não é igual")
+}
+
+numero = 40;
+if (30 >= numero){
+    console.log("é maior")
+} else if(30 != numero){
+    console.log("é diferente")
+} */
+
+const numero = 6
+if (numero > 2 || numero < 6){
+    console.log("está no intervalo")
+}else{
+    console.log("não está no intervalo")
+}
