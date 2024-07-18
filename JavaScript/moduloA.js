624c8a8d87adf1ee021564c2544a447ec0e1ae19
David Augusto <david11augusto11@gmail.com>
2024-07-17 20:28:06 -0400

    módulos

--- JavaScript/moduloA.js
+++ JavaScript/moduloA.js
@@ -1,5 +1,38 @@
-export function olaMundo(){
-    console.log("Olá mundo")
+/* const nome = "David";
+const idade = 21;
+
+function soma(a, b){
+    return a+b
 }
 
-module.exports = olaMundo
+const PI = 3.14
+
+function areaCirculo(raio){
+    return PI * raio ** 2
+}
+
+module.exports={
+    nome, idade, soma, areaCirculo, PI 
+}
+ */
+/* const pessoa = {
+    nome: "David",
+    idade: 22,
+    cidade: "Manaus"
+}
+
+module.exports = pessoa */
+
+const array = [
+    {
+        time: "Vasco",
+        titulo: "Campeão da série B"
+    },
+
+    {
+        time: "Flamengo",
+        titulo: "tem um time top e não ganha nada"
+    }
+]
+
+module.exports = array
--- JavaScript/moduloB.js
+++ JavaScript/moduloB.js
@@ -1,5 +1,20 @@
-//const moduloA = require('./moduloA')
-import { olaMundo } from "./moduloA"
+/* const {soma, areaCirculo, PI} = require('./moduloA') // desestruturação
 
+console.log("Area do circulo (r = 2): "+areaCirculo(2))
+console.log(PI) */
+/* const moduloA = require('./moduloA')
 
-moduloA.olaMundo()
+console.log(moduloA)
+console.log(moduloA.nome)
+console.log(moduloA.idade)
+console.log(moduloA.cidade)
+
+ */
+
+const moduloA = require('./moduloA')
+
+moduloA.map(
+    (item)=>{
+        console.log(item.time)
+    }
+)
