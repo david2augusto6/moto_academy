6307c068000244bad84955ac32763084beae2f1c
David Augusto <david11augusto11@gmail.com>
2024-08-07 20:22:45 -0400

    Aula express resquisição e resposta

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+
+const users = [
+    {id:1, name: "João"},
+    {id:2, name: "David"},
+]
+
+app.get('/usuarios', (req, res)=>{
+    res.json(users)
+})
+
+app.post('/usuarios', (req, res)=>{
+    const name = req.body.name
+    const id = users.length+1
+
+    const newUser = {id, name}
+    users.push(newUser)
+    res.status(201).json(newUser)
+})
 
 app.get('/', (req, res) =>{
     res.send({message: "olá mundo"})
 })
 
-app.listen(3000)
+app.listen(3002)
