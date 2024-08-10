9bf00c35e289df0c68b64005bc197879faebdb59
David Augusto <david11augusto11@gmail.com>
2024-08-09 20:11:58 -0400

    requisições PUT e DELETE

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 
 const app = express()
+
 app.use(express.json())
 
-const users = [
-    {id:1, name: "João"},
-    {id:2, name: "David"},
+let users = [
+    {id: 1, name: "João"},
+    {id: 2, name: "David"},
 ]
 
 app.get('/usuarios', (req, res)=>{
-    res.json(users)
+    res.status(200).json(users)
 })
 
 app.post('/usuarios', (req, res)=>{
@@ -21,8 +22,27 @@ app.post('/usuarios', (req, res)=>{
     res.status(201).json(newUser)
 })
 
+app.put("/usuarios/:id", (req, res)=>{
+    const id = parseInt(req.params.id)
+    const {name} = req.body
+    const userIndex = users.findIndex(user => user.id == id)
+    if (userIndex != -1){
+        users[userIndex].name = name
+        res.json(users[userIndex])
+    }else{
+        res.status(400).json({message: "Usuario não encontrado"})
+    }
+    
+})
+
+app.delete('/usuarios/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    users = users.filter(user=>user.id !== id)
+    res.sendStatus(204)
+})
+
 app.get('/', (req, res) =>{
     res.send({message: "olá mundo"})
 })
 
-app.listen(3002)
+app.listen(3000)
